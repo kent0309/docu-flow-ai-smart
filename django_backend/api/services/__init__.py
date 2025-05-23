7dec389293ea6a647db682ce8519e86280088916
kent0309 <148470863+kent0309@users.noreply.github.com>
2025-05-23 17:11:09 +0800

    Delete django_backend directory

--- django_backend/api/services/__init__.py
+++
@@ -1,4 +0,0 @@
-
-# Import services to make them available when importing the services package
-from .document_processor import extract_data, summarize_document, train_ml_model
-from .ml_document_classifier import document_classifier
--- django_backend/document_processor/__init__.py
+++
@@ -1,2 +0,0 @@
-
-# Initialize document processor project
